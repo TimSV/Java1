668271b83fb4432c65cc87f4ea6be9083544bb79
Sergey Timofeev <72862210+TimSV@users.noreply.github.com>
2020-10-20 20:02:58 +0300

    Completed Tic Tac Toe Game

--- src/lesson4/ImprovedTicTacToe.java
+++ src/lesson4/ImprovedTicTacToe.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class ImprovedTicTacToe {
 
-    static final int SIZE = 5;
+    static final int SIZE = 3;
     static final int CHIPS = 3;
 
     static final char DOT_EMPTY = '•';
@@ -154,23 +154,22 @@ public class ImprovedTicTacToe {
             for (int j = 0; j < SIZE; j++) {
                 row[j] = map[i][j];
             }
-            if (checkRow(row,symbol)) return true;
+            if (checkRow(row, symbol)) return true;
         }
         for (int j = 0; j < SIZE; j++) {
             for (int i = 0; i < SIZE; i++) {
                 row[i] = map[i][j];
             }
-            if (checkRow(row,symbol)) return true;
+            if (checkRow(row, symbol)) return true;
         }
         for (int i = 0, j = 0; i < SIZE; i++, j++) {
             row[i] = map[i][j];
         }
-        if (checkRow(row,symbol)) return true;
+        if (checkRow(row, symbol)) return true;
         for (int i = 0, j = SIZE - 1; i < SIZE; i++, j--) {
             row[i] = map[i][j];
         }
-        if (checkRow(row,symbol)) return true;
-        return false;
+        return checkRow(row, symbol);
     }
 
     private static boolean isMapFull() {
@@ -186,15 +185,76 @@ public class ImprovedTicTacToe {
 
 
     private static void aiTurn() {
-        int rowNumber;
+        int rowNumber ;
         int colNumber;
+        char[] row = new char[SIZE];
+
         System.out.println("\nХод компьютера!\n");
+
+        for (int i = 0; i < SIZE; i++) {
+            rowNumber = i;
+            colNumber = checkRowAi(map[i]);
+            if (colNumber > -1) {
+                map[rowNumber][colNumber] = DOT_AI;
+                return;
+            }
+        }
+
+        for (int j = 0; j < SIZE; j++) {
+            colNumber = j;
+            for (int i = 0; i < SIZE; i++) {
+                row[i] = map[i][j];
+            }
+            rowNumber = checkRowAi(row);
+            if (rowNumber > -1) {
+                map[rowNumber][colNumber] = DOT_AI;
+                return;
+            }
+        }
+
+        for (int i = 0, j = 0; i < SIZE; i++, j++) {
+            row[i] = map[i][j];
+        }
+        colNumber = checkRowAi(row);
+        if (colNumber > -1) {
+            map[colNumber][colNumber] = DOT_AI;
+            return;
+        }
+
+        for (int i = 0, j = SIZE - 1; i < SIZE; i++, j--) {
+            row[i] = map[i][j];
+        }
+        colNumber = checkRowAi(row);
+        if (colNumber > -1) {
+            map[colNumber][SIZE - colNumber - 1] = DOT_AI;
+            return;
+        }
+
         do {
             rowNumber = random.nextInt(SIZE) + 1;
             colNumber = random.nextInt(SIZE) + 1;
         } while (!isCellValid(rowNumber, colNumber, true));
-
         map[rowNumber - 1][colNumber - 1] = DOT_AI;
+
+    }
+
+    private static int checkRowAi(char[] row) {
+        int index = -1, cnt = 0;
+
+        for (int k = 0; k <= SIZE - CHIPS; k++) {
+            int count = 0;
+            for (int i = k; i < CHIPS + k; i++) {
+                if (row[i] == DOT_HUMAN)
+                    count++;
+                if (row[i] == DOT_EMPTY)
+                    index = i;
+            }
+            if (count >= CHIPS - 1) {
+                cnt = count;
+                break;
+            }
+        }
+        return (cnt >= CHIPS - 1) ? index : -1;
     }
 
     private static boolean checkRow(char[] row, char symbol) {
@@ -205,7 +265,7 @@ public class ImprovedTicTacToe {
             for (int i = 0; i < buf.length; i++) {
                 buf[i] = row[i + k];
             }
-            if (Arrays.equals(buf, win)) res =  true;
+            if (Arrays.equals(buf, win)) res = true;
         }
         return res;
     }
